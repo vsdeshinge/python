b489be3a6e8bd8bc60e36c6c0c27cccd68c7f1e0
vsdeshinge <106578013+vsdeshinge@users.noreply.github.com>
2022-06-01 16:26:57 +0000

    hangman

--- hangman.py
+++ hangman.py
@@ -1,75 +1,16 @@
 # Step 1
 
 import random
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["aardvark", "baboon", "camel"]
+import hangman_art
+import hangman_words
 maharshi = []
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+print("Wel-come to ")
+print(hangman_art.logo)
+print("game\n")
+chosen_word = random.choice(hangman_words.word_list)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-chosen_word = random.choice(word_list)
-y = []
-for x in chosen_word:
-    y.append(x)
 
 for x in range(len(chosen_word)):
     maharshi += "_"
@@ -79,21 +20,26 @@ m = 0
 endgame = False
 while not endgame:
     maggy = str(input("guess a letter  "))
+    if maggy in maharshi:
+        print("you already entered the letter")
+        
 
     count = 0
     for x in chosen_word:
 
         if x == maggy:
             maharshi[count] = x
-        if maggy not in y:
+        if maggy not in chosen_word:
             m+=1
+            
 
         count += 1
     print(maharshi)
     t=int(m/len(chosen_word))
-    print(stages[6-t])
+    print(hangman_art.stages[6-t])
     if t==6:
         print("you lost")
+        print(chosen_word)
         break
 
     if "_" not in maharshi:
